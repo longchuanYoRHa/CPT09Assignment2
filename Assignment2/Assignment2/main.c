fe8ac15a3ba723059e417baa60344d0513c2aa22
Longchuan <2674312287@qq.com>
2022-11-11 11:42:44 +0800

    Designing the structure

---
+++ Assignment2/Assignment2/main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+
+
+int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevinstance, PSTR szCmdLine, int iCmdShow)
+{
+	//display window
+	//input user name and password
+	//choose if to delete the user data
+	return 0;
+}
