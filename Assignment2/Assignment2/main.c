b1d592a61c4465cce2c635b4bd4e0831778edd01
Longchuan <2674312287@qq.com>
2022-11-17 12:52:15 +0800

    User init

--- Assignment2/Assignment2/main.c
+++ Assignment2/Assignment2/main.c
@@ -1,37 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+#include<Windows.h>
+//#include"testsource.h"
 
 #define COL 3
-
-
-
-//menu
-int menu()
+#define ROW 3
+//window size
+void WindowSize(unsigned uCol, unsigned uLine)
 {
-	printf("###########################\n");
-	printf("##   1.Play	2.Exit   ##\n");
-	printf("###########################\n");
-	return 0;
+	char cmd[64];
+	sprintf(cmd, "mode con cols=%d lines=%d", uCol, uLine);
+	system(cmd);
 }
+
+//create file for login
+void Init_file()
+{
+	FILE *fp;
+	if ((fp = fopen("users.txt", "rb")) == NULL)
+	{
+		if ((fp = fopen("users.txt", "wb+")) == NULL)
+		{
+			printf("faild to create user data file!\n");
+			exit(0);
+		}
+	}
+}
+//menu
+
 
 //display gameboard
-int ShowBroad(char chess[][COL])
-{
-	system("cls");		//Ë¢ÐÂ½çÃæ
-	printf(" %c | %c |%c          Player:   X\n", chess[0][0], chess[0][1], chess[0][2]);
-	printf(" ----------        Computer: O\n");
-	printf(" %c | %c |%c \n", chess[1][0], chess[1][1], chess[1][2]);
-	printf(" ----------\n");
-	printf(" %c | %c |%c \n", chess[2][0], chess[2][1], chess[2][2]);
 
-}
 
+//init
+int a, count = 0;
+char chess[ROW][COL] = {'\0'};
 
 int main()
 {
+	WindowSize(50, 15);
 	//determain if there's already a file created
 	//establish the file for user behavior and account storage
+	// 
+	Init_file();
+	//set cursor
+	
 	//display user interface
 	//register or login
 	// if register
---
+++ Assignment2/Assignment2/usermanagment.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _USERMANAGMENT_H
+#define _USERMANAGMENT_H
+typedef struct User_Data
+{
+	char ID[10];
+	char psw[10];
+}users;
+
+
+void user_register()
+
+void user_login()
+
+#endif // !_USERMANAGMENT_H
